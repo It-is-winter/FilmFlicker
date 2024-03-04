ef6984a4cec1646f01c1e40bdb93d2c65bc6f601
It-is-winter <seaotter.theatre@gmail.com>
2024-03-04 15:43:35 +0900

    UsersSession, UsersSessionSet 기본 내용

--- src/session/UsersSession.java
+++ src/session/UsersSession.java
@@ -1,5 +1,40 @@
 package session;
 
-public interface UsersSession {
+import java.util.HashMap;
+import java.util.Map;
+
+public class UsersSession {
+	private String sessionId; //usersId
+	private Map<String, Object> attributes; //찜 목록, 리뷰 삭제
+	
+	public UsersSession() {}
+	public UsersSession(String sessionId) {
+		this.sessionId = sessionId;
+		attributes = new HashMap<String, Object>();
+	}
+	public String getSessionId() {
+		return sessionId;
+	}
+	public void setSessionId(String sessionId) {
+		this.sessionId = sessionId;
+	}
+	public Map<String, Object> getAttributes() {
+		return attributes;
+	}
+	public void setAttributes(Map<String, Object> attributes) {
+		this.attributes = attributes;
+	}
+	@Override
+	public int hashCode() {
+		return sessionId.hashCode();
+	}
+	@Override
+	public boolean equals(Object obj) {
+		UsersSession other = (UsersSession) obj;
+		if(sessionId.equals(other.sessionId)) {
+			return true;
+		}
+		return false;
+	}
 
 }
--- src/session/UsersSessionSet.java
+++ src/session/UsersSessionSet.java
@@ -1,5 +1,44 @@
 package session;
 
-public interface UsersSessionSet {
+import java.util.HashSet;
+import java.util.Set;
+
+public class UsersSessionSet {
+	private static UsersSessionSet usersSessionSet = new UsersSessionSet();
+	private Set<UsersSession> set;
+	
+	private UsersSessionSet() {
+		set = new HashSet<UsersSession>();
+	}
+	
+	public static UsersSessionSet getInstance() {
+		return usersSessionSet;
+	}
+	
+	/**
+	 * 사용자 찾기
+	 * */
+	public UsersSession get(String sessionId) {
+		for(UsersSession s : set) {
+			if(s.getSessionId().equals(sessionId)) {
+				return s;
+			}
+		}
+		return null;
+	}
+	
+	/**
+	 * 로그인된 사용자 추가
+	 * */
+	public void add(UsersSession usersSession) {
+		set.add(usersSession);
+	}
+	
+	/**
+	 * 로그아웃한 사용자 제거
+	 * */
+	public void remove(UsersSession usersSession) {
+		set.remove(usersSession);
+	}
 
 }
