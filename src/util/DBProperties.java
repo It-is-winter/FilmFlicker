4a3e566c79233d468e12c11eb22dd14a20bdcfc3
It-is-winter <seaotter.theatre@gmail.com>
2024-03-06 09:39:34 +0900

    DbManger 수정 , DBProperties 추가

---
+++ src/util/DBProperties.java
@@ -0,0 +1,10 @@
+package util;
+
+public interface DBProperties {
+
+			String driverName="oracle.jdbc.driver.OracleDriver";
+			String url="jdbc:oracle:thin:@127.0.0.1:1521:XE";
+			String userName="c##test";
+			String userPass="1234";
+	
+}
--- src/util/DbManager.java
+++ src/util/DbManager.java
@@ -1,6 +1,5 @@
 package util;
 
-import java.io.FileInputStream; 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
