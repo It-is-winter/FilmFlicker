00aa181b287a58a9a54ac4cb2f1ce2c334e8b99f
hongsoonho0723 <ghstns357@gmail.com>
2024-03-06 16:28:19 +0900

    dbmanager commit

---
+++ src/util/DbManager.java
@@ -0,0 +1,53 @@
+package util;
+
+import java.io.FileInputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
+
+public class DbManager {
+	private static Properties proFile = new Properties();
+
+	/**
+	 * 로드
+	 */
+	static {
+		try {
+			
+			
+			Class.forName(DBProperties.DRIVER_NAME);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	
+	public static Properties getProFile() {
+		return proFile;
+	}
+
+	public static Connection getConnection() throws SQLException {
+		return DriverManager.getConnection(
+				DBProperties.URL,
+				DBProperties.USER_ID,
+				DBProperties.USER_PASS);
+				
+	}
+	
+
+	public static void close(Connection con, Statement st, ResultSet rs) {
+		try {
+			if(rs != null) rs.close();
+			if(st != null) st.close();
+			if(con != null) con.close();
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
